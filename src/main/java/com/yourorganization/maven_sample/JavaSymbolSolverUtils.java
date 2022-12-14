540a191195cdcb61c3ab66cf745e015d09b2da29
mahidedhia <mahi.dedhia@somaiya.edu>
2022-12-14 23:33:07 +0530

    Extracting Qualified name from NameExpr, Field Variable
    Co-authored-by: Isha <isha.dhabalia@somaiya.edu>

--- src/main/java/com/yourorganization/maven_sample/JavaSymbolSolverUtils.java
+++ src/main/java/com/yourorganization/maven_sample/JavaSymbolSolverUtils.java
@@ -4,8 +4,17 @@ import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.AssignExpr;
+import com.github.javaparser.ast.expr.FieldAccessExpr;
+import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 import com.github.javaparser.symbolsolver.JavaSymbolSolver;
@@ -33,15 +42,25 @@ public class JavaSymbolSolverUtils {
         return cu;
     }
 
-    public void getQualifiedNameMethodCall(CompilationUnit cu){
-        ArrayList<String> qualifiedNames = new ArrayList<String>();
-        cu.findAll(MethodCallExpr.class ).forEach(methodCall -> {
-            ResolvedMethodDeclaration methodDeclaration = methodCall.resolve();
-            System.out.println("Method: " + methodDeclaration.getQualifiedName());
-            System.out.println("Method " + methodDeclaration.getQualifiedSignature());
-            System.out.println("Method " + methodCall.calculateResolvedType());
-            qualifiedNames.add(methodDeclaration.getQualifiedName());
-        });
-//        return qualifiedNames;
+    public String getQualifiedName(Node node){
+        if(node instanceof NameExpr){
+            ResolvedValueDeclaration rvd = ((NameExpr) node).resolve();
+            if(rvd.isField()){
+                ResolvedFieldDeclaration rfd = rvd.asField();
+                Optional<FieldDeclaration> fdec = rfd.toAst();
+                ClassOrInterfaceDeclaration ciDec = (ClassOrInterfaceDeclaration) fdec.get().getParentNode().get();
+                String nodeQualifiedName = ciDec.getNameAsString() + "." + ((NameExpr) node).getNameAsString();
+                return nodeQualifiedName;
+            }
+            return ((NameExpr) node).getNameAsString();
+        }
+
+        else if(node instanceof MethodCallExpr){
+            ResolvedMethodDeclaration methodDeclaration = ((MethodCallExpr) node).resolve();
+            return methodDeclaration.getQualifiedName();
+        }
+
+        return "";
     }
+
 }
